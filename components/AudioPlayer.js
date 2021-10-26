797ef68ea5011b2d033d7d7ab5ae34c31e12a053
Carlos <krloslao90@gmail.com>
2021-10-26 13:37:13 -0400

    upd

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -7,7 +7,7 @@ function AudioPlayer({cuePlay}) {
         cuePlay ? player.play() : player.pause();
     }, [cuePlay])
 
-    return (<audio id="audio" src="https://melocue.com/radio/8000/radio.mp3" preload="auto" type="audio/mpeg"></audio>)   
+    return (<audio id="audio" src="http://159.223.124.77/radio/8000/radio.mp3" preload="auto" type="audio/mpeg"></audio>)   
 }
 
 export default AudioPlayer
