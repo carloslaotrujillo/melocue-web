e2ccd482e596402d88c2fc79102960c9233953dc
Carlos <krloslao90@gmail.com>
2021-10-26 10:52:57 -0400

    update build

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7b17fcbf3b217c7484bb";
+/******/ 	var hotCurrentHash = "36e75cc88fe4ba328b0c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
