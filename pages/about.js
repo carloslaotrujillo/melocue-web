ea1355941ffbe667836ffbdadb65cad3eb036b28
Carlos Trujillo <krloslao90@gmail.com>
2023-04-23 00:19:16 -0400

    updating npm & changing email

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,7 @@ function About() {
         Melocue is a non-profit project. Any content broadcasted here is subject
         to individual copyright of the author and is not the purpose of Melocue
         and its products to profit from it. Form more information contact us at
-        melocue@gmail.com.
+        info@melocue.com.
       </p>
     </div>
   );
