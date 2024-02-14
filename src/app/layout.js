57d7c0af55f3abbaea444b8672a55ccf9eb5da4b
Charles La O <krloslao90@gmail.com>
2024-02-14 18:58:50 -0500

    adding speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import NavBar from "./_components/NavBar/NavBar";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
 				<NavBar />
 				<main className="flex-grow">{children}</main>
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
