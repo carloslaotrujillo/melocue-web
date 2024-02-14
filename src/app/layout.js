8f65a87bbc6387b9277641dc522e11fae2280cf7
Charles La O <krloslao90@gmail.com>
2024-02-14 18:54:28 -0500

    adding vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import NavBar from "./_components/NavBar/NavBar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
 			<body className={inter.className + " flex flex-col min-h-screen"}>
 				<NavBar />
 				<main className="flex-grow">{children}</main>
+				<Analytics />
 			</body>
 		</html>
 	);
