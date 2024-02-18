a2dde1f3f472a79a49ffab5afd1a109b963c83e0
Charles La O <krloslao90@gmail.com>
2024-02-18 16:03:44 -0500

    solving error in terms of service text

--- src/app/terms-of-service/page.js
+++ src/app/terms-of-service/page.js
@@ -6,20 +6,21 @@ function TermsOfService() {
 			<h1>Terms of Service</h1>
 			<p>
 				Melocue Terms of Service Last Updated: January 01, 2024 1. Introduction Welcome to Melocue! These Terms of
-				Service ("Terms") govern your use of our website located at melocue.com ("Site"), and any related services
-				provided by JC&X Solutions LLC ("Company," "we," "us," or "our"). 2. Agreement to Terms By accessing our Site or
-				using our services, you agree to these Terms and our Privacy Policy. If you disagree with any part of the Terms,
-				you may not access the Site. 3. Changes to Terms We reserve the right to modify these Terms at any time. Your
-				continued use of the Site after any such change constitutes your acceptance of the new Terms. 4. Services The
-				Services are provided as-is, and are intended for users who are at least 18 years old. They are not tailored to
-				comply with industry-specific regulations such as HIPAA, FISMA, or GLBA. 5. User Registration You may need to
-				register to use our Site. You agree to keep your password confidential and will be responsible for all use of
-				your account. 6. Intellectual Property All content on the Site is the property of Melocue LLC or its licensors
-				and is protected by intellectual property laws. 7. User Contributions By posting content to the Site, you grant
-				us a license to use that content. You must have the rights to the content you post and must not post offensive
-				or prohibited content. 8. Prohibited Activities You may not use the Site for any illegal activities, and you may
-				not violate others' rights or privacy. 9. Purchases and Payments We accept various forms of payment. You agree
-				to provide accurate and complete purchase and account information. 10. Third-Party Content Our Site may contain
+				Service (&ldquo;Terms&rdquo;) govern your use of our website located at melocue.com (&ldquo;Site&rdquo;), and
+				any related services provided by JC&X Solutions LLC (&ldquo;Company,&rdquo; &ldquo;we,&rdquo; &ldquo;us,&rdquo;
+				or &ldquo;our&rdquo;). 2. Agreement to Terms By accessing our Site or using our services, you agree to these
+				Terms and our Privacy Policy. If you disagree with any part of the Terms, you may not access the Site. 3.
+				Changes to Terms We reserve the right to modify these Terms at any time. Your continued use of the Site after
+				any such change constitutes your acceptance of the new Terms. 4. Services The Services are provided as-is, and
+				are intended for users who are at least 18 years old. They are not tailored to comply with industry-specific
+				regulations such as HIPAA, FISMA, or GLBA. 5. User Registration You may need to register to use our Site. You
+				agree to keep your password confidential and will be responsible for all use of your account. 6. Intellectual
+				Property All content on the Site is the property of Melocue LLC or its licensors and is protected by
+				intellectual property laws. 7. User Contributions By posting content to the Site, you grant us a license to use
+				that content. You must have the rights to the content you post and must not post offensive or prohibited
+				content. 8. Prohibited Activities You may not use the Site for any illegal activities, and you may not violate
+				others&apos; rights or privacy. 9. Purchases and Payments We accept various forms of payment. You agree to
+				provide accurate and complete purchase and account information. 10. Third-Party Content Our Site may contain
 				links to third-party websites or content. We are not responsible for third-party content, and you access it at
 				your own risk. 11. Disclaimers We do not guarantee the accuracy or completeness of any information on our Site,
 				and we are not responsible for any errors or omissions. 12. Limitation of Liability Our liability to you is
